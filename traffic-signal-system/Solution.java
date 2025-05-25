5a19fd35540bf79ff623c811f42c3eb92803b504
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2025-05-25 19:11:25 +0100

    traffic-signal-system solution

--- traffic-signal-system/Solution.java
+++ traffic-signal-system/Solution.java
@@ -6,7 +6,12 @@
 // Extensibility: Easy to add new directions or states if needed.
 // State Pattern: Use the State design pattern to encapsulate state-specific behavior and transitions.
 
+import java.time.Duration;
 import java.util.EnumMap;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TrafficLight implements Subject {
 
@@ -14,8 +19,16 @@ public class TrafficLight implements Subject {
     private final Direction direction;
     private final List<Observer> observers;
 
+    public void setState(State state) {
+        context.setState(state);
+    }
+
     @Override
-    public void notifyAll() {}
+    public void notifyAll() {
+        for (var obs : observers) {
+            obs.updated(this);
+        }
+    }
 }
 
 public interface Subject {
@@ -24,12 +37,21 @@ public interface Subject {
 
 public class Context {
 
-    private State state;
+    private volatile State state;
     private final TrafficLight trafficLight;
+    private final IntersectionManager intersectionManager;
+
+    public void scheduleTransition(Duration delay, State newState) {
+        intersectionManager.scheduleTransition(delay, trafficLight, newState);
+    }
 
     void setState(State state) {
         this.state = state;
+        state.onStateSet();
+        trafficLight.notifyAll();
     }
+
+    Yellow getYellow();
 }
 
 public abstract class State {
@@ -44,33 +66,56 @@ public abstract class State {
     public abstract boolean isOpen();
 
     public abstract boolean isClosed();
+
+    abstract void onStateSet();
 }
 
 public class Green implements State {
+
     @Override
-    public void open() {
-        final var
-    }
+    public void open() {}
 
     @Override
     public void close() {
         final var context = getContext();
-        context.setState(context.getYellow());
+        context.setState(context.getRed());
     }
 
     @Override
-    public boolean isClose() {
+    public boolean isClosed() {
         return false;
+    }
+
+    @Override
+    void onStateSet() {
+        context.scheduleTransition(duration, context.getYellow());
     }
 }
 
 public class Yellow implements State {
+
     public Yellow(Duration duration, Context context) {
         super(duration, context);
     }
+
+    @Override
+    void onStateSet() {
+        context.scheduleTransition(duration, context.getRed());
+    }
 }
 
-public class Red implements State {}
+public class Red implements State {
+
+    @Override
+    public void open() {
+        final var context = getContext();
+        final var newState = context.getGreen();
+        context.setState(newState);
+    }
+
+    @Override
+    void onStateSet() {}
+}
 
 public enum Direction {
     NORTH,
@@ -83,9 +128,25 @@ public class IntersectionManager implements Observer {
 
     private final EnumMap<Direction, TrafficLight> trafficLights;
     private Strategy strategy;
+    private final ScheduledExecutorService executor;
 
     public IntersectionManager() {
         trafficLights = new EnumHashMap<>(Direction.class);
+        executor = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    public void scheduleTransition(
+        Duration delay,
+        TrafficLight trafficLight,
+        State newState
+    ) {
+        executor.schedule(
+            () -> {
+                trafficLight.setState(newState);
+            },
+            delay.toSeconds(),
+            TimeUnit.SECONDS
+        );
     }
 
     public void setNorth(TrafficLight trafficLight) {}
@@ -114,15 +175,25 @@ public interface Strategy {
 
 public class RoundRobin implements Strategy {
 
-    public RoundRobin(List<Direction> available) {}
+    private final List<Direction> directions;
+    private final EnumMap<Direction, Direction> next;
+
+    public RoundRobin(List<Direction> available) {
+        this.directions = List.copyOf(available);
+        this.next = new EnumHashMap<>(Direction.class);
+        if (directions.size() == 1) {
+            final var uniqueDirection = directions.get(0);
+            next.put(uniqueDirection, uniqueDirection);
+            return;
+        }
+        for (var i = 1; i < directions.size(); ++i) {
+            next.put(directions.get(i - 1), directions.get(i));
+        }
+        next.put(directions.size() - 1, directions.get(0));
+    }
 
     @Override
     public Direction next(Direction closingDirection) {
-        return switch (closingDirection) {
-            case NORTH -> EAST;
-            case EAST -> SOUTH;
-            case SOUTH -> WEST;
-            case WEST -> NORTH;
-        };
+        return next.get(closingDirection);
     }
 }
