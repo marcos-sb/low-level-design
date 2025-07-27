50b0593b2846f9f99cb71e2bfeed0a00a54ad620
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2025-07-27 18:55:41 +0100

    hotel management system

--- hotel-management-system/Solution.java
+++ hotel-management-system/Solution.java
@@ -1,5 +1,7 @@
+import java.time.LocalDate;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.TimeUnit;
 
 public class Room {
 
@@ -74,7 +76,6 @@ public class System {
             reservation.checkin(),
             reservation.checkout()
         );
-        reservation.room().status(Room.Status.AVAILABLE);
     }
 
     public void checkIn(Reservation reservation) {
@@ -83,6 +84,10 @@ public class System {
 
     public void checkOut(Reservation reservation) {
         reservation.status(Reservation.Status.CHECKED_OUT);
+    }
+
+    public void takeRoomForMaintenance(Room room) {
+        inventory.takeRoomForMaintenance(room);
     }
 }
 
@@ -107,7 +112,9 @@ public class Inventory {
         LocalDate,
         ConcurrentSkipListSet<Room>
     > availableRoomsByDate;
+    private LocalDate latestDateInAvailableRoomsByDate;
     private final ConcurrentSkipListSet<Room> allRooms;
+    private final ConcurrentSkipListSet<Room> roomsInMaintenance;
 
     public synchronized void addRoom(Room room) {
         allRooms.add(room);
@@ -121,6 +128,9 @@ public class Inventory {
         LocalDate checkin,
         LocalDate checkout
     ) {
+        if (roomsInMaintenance.contains(room)) {
+            throw new IllegalStateException("Room is under maintenance");
+        }
         for (
             var currDate = checkin;
             currDate.before(checkout);
@@ -135,6 +145,9 @@ public class Inventory {
         LocalDate checkin,
         LocalDate checkout
     ) {
+        if (!allRooms.contains(room)) {
+            throw new NoSuchElement("Room not found in inventory");
+        }
         for (
             var currDate = checkin;
             currDate.before(checkout);
@@ -143,17 +156,119 @@ public class Inventory {
             availableRoomsByDate.get(currDate).add(room);
         }
     }
+
+    public synchronized void takeRoomForMaintenance(Room room) {
+        if (!allRooms.contains(room)) {
+            throw new NoSuchElement("Room not found in inventory");
+        }
+        if (roomsInMaintenance.contains(room)) return;
+        for (var entry : availableRoomsByDate.entrySet()) {
+            entry.getValue().remove(room);
+        }
+        roomsInMaintenance.add(room);
+    }
+
+    public synchronized void extendInventory(LocalDate endDate) {
+        for (
+            var currDate = latestDateInAvailableRoomsByDate;
+            currDate.before(endDate);
+            currDate.plus(TimeUnit.DAYS)
+        ) {
+            availableRoomsByDate.putIfAbsent(
+                currDate,
+                new ConcurrentSkipListSet<>()
+            );
+        }
+        latestDateInAvailableRoomsByDate = endDate;
+    }
+
+    public synchronized void pruneInventory(LocalDate startDate) {
+        if (startDate.isAfter(latestDateInAvailableRoomsByDate)) {
+            availableRoomsByDate.clear();
+            return;
+        }
+        for (var date : availableRoomsByDate.keySet()) {
+            if (date.isBefore(startDate)) {
+                availableRoomsByDate.remove(date);
+            }
+        }
+        latestDateInAvailableRoomsByDate = startDate;
+    }
 }
 
 public record Guest(String name, String email, String phone) {
     public record ID(String value) {}
 }
 
-public class PaymentProcessor {}
+public class PaymentProcessor {
+
+    public Invoice settlePayment(
+        Reservation reservation,
+        PaymentMethod paymentMethod
+    ) {
+        final var totalAmount = calculateTotal(reservation);
+        final var issuedAt = Instant.now();
+        final var invoice = new Invoice(
+            reservation.guest(),
+            reservation,
+            totalAmount,
+            issuedAt,
+            paymentMethod
+        );
+
+        try {
+            paymentMethod.charge(totalAmount);
+        } catch (PaymentException pe) {
+            throw new IllegalStateException("Payment failed", pe);
+        }
+
+        return invoice;
+    }
+
+    private Money calculateTotal(Reservation reservation) {
+        final var days = ChronoUnit.DAYS.between(
+            reservation.checkin(),
+            reservation.checkout()
+        );
+        return reservation.room().rate().multiply(days);
+    }
+}
 
 public record Invoice(
     Guest guest,
     Reservation reservation,
     Money totalAmount,
-    Instant issuedAt
+    Instant issuedAt,
+    PaymentMethod paymentMethod
 ) {}
+
+public interface PaymentMethod {
+    void charge(Money amount);
+}
+
+public class CreditCardPayment implements PaymentMethod {
+
+    private final String cardNumber;
+    private final String cardHolderName;
+    private final String expiryDate;
+    private final String cvv;
+
+    @Override
+    public void charge(Money amount) throws PaymentException {}
+}
+
+public class BankTransferPayment implements PaymentMethod {
+
+    private final String bankAccountNumber;
+    private final String bankName;
+
+    @Override
+    public void charge(Money amount) throws PaymentException {}
+}
+
+public class PaymentException extends Exception {
+
+    public PaymentException(String message) {
+        super(message);
+    }
+}
